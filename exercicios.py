53ec181e77e09c907e03798947fbe8875dd352e6
Marcus Bragion <viniciusbragion@outlook.pt>
2024-07-13 11:48:29 -0300

    feat(exercicios) adicionado novas respostas

--- exercicios.py
+++ exercicios.py
@@ -1,10 +1,32 @@
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+valor_1 = int(input("Digite primeiro valor: "))
+valor_2 = int(input("Digite segundo valor: "))
+resultado = valor_1 + valor_2
+print(f"Resultado: {resultado}")
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+valor = int(input("Digite primeiro valor: "))
+denominador = 5
+resultado = valor / denominador
+print(f"Resultado da divisão por {denominador}: {resultado}")
+
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+valor = int(input("Digite primeiro valor: "))
+multiplicador = int(input("Defina o multiplicador: "))
+resultado = valor * multiplicador
+print(f"Resultado: {resultado}")
+
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+valor_1 = int(input("Digite um número inteiro : "))
+valor_2 = int(input("Digite outro número inteiro: "))
+resultado = valor_1 / valor_2
+print(f"Resultado: {resultado}")
+
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+valor = int(input("Digite um número: "))
+calcular_quadrado = valor ** 2
+print(f"Resultado: {resultado}")
 
 # #### Números de Ponto Flutuante (`float`)
 
